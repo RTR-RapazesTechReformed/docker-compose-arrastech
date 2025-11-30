55fac1e08817f4b0d9f61a7f3cb3722f34929a0c
Edson Morro Nogueira <125297431+EdsonMorroNogueira@users.noreply.github.com>
2025-11-29 22:57:10 -0300

    feat(app): .sh principal da montagem do 2º Semes

---
+++ montar.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Por favor, execute como root ou usando sudo."
+    exit 1
+fi
+
+sudo apt update -y && sudo apt upgrade -y
+
+if ! command git -v  &> /dev/null; then
+    echo "🐱 Instalando Git..."
+    sudo apt install git -y
+else
+    echo "✅ Git já está instalado."
+fi
+
+echo "✅ Git instalado com sucesso!"
+
+if ! command docker -v  &> /dev/null; then
+    echo "🐋 Instalando Docker..."
+    sudo apt update -y && apt install -y docker.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    echo "✅ Docker instalado com sucesso!"
+else
+    echo "✅ Docker já está instalado."
+fi
+
+echo "✅ Docker instalado com sucesso!"
+
+if ! command docker-compose -v &> /dev/null; then
+    echo "🐋 Instalando Docker Compose..."
+    sudo apt install docker-compose -y
+else
+    echo "✅ Docker Compose já está instalado."
+fi
+
+
+echo "📥 Clonando repositórios..."
+git clone -b feature/deploy-init-actions https://github.com/RTR-RapazesTechReformed/storemanager-frontend.git
+git clone -b feature/dockerfile-env https://github.com/RTR-RapazesTechReformed/store-manager-api.git
+git clone https://github.com/RTR-RapazesTechReformed/docker-compose-arrastech.git
+
+echo "🚀 Subindo os containers com Docker Compose..."
+cd docker-compose-arrastech
+cp docker-compose.yml ..
+cd ..
+sudo docker-compose up --build -d
+
+echo "🧹 Removendo repositórios clonados..."
+
+rm -rf docker-compose-arrastech front-end-arrastech back-end-arrastech docker-compose.yml
+
+echo "✅ Configuração concluída!"
+echo "Agora você pode rodar contêineres conectados à rede privada."
+
+exit 0
